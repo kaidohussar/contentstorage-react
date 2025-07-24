efab594ceaf9feed190276737014ff540850b9fd
Kaido Hussar <kaidohus@gmail.com>
2025-07-24 10:09:05 +0300

    up

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,6 @@ import type { LanguageCode } from '@contentstorage/core';
 
 export * from './components/ContentProvider';
 export * from './components/Text';
+export * from './components/Variation';
+export * from './components/Image';
 export { LanguageCode };
